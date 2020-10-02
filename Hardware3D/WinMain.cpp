b791033b4730963d5976bceda53976885a84a3a7
Foon <projectraccoon@mailbox.org>
2020-10-02 06:55:50 -0500

    Added a basic Window handling class and added a setup header

--- Hardware3D/WinMain.cpp
+++ Hardware3D/WinMain.cpp
@@ -1,17 +1,4 @@
-#include <Windows.h>
-
-//Custom windows procedure
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	switch (msg)
-	{
-	case WM_CLOSE:
-		//Return code will be 81 (Q) if user exits out
-		PostQuitMessage('Q');
-		break;
-	}
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
+#include "Window.h"
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
@@ -19,39 +6,12 @@ int CALLBACK WinMain(
 	LPSTR lpCmdLine, 
 	int nCmdShow)
 {
-	const wchar_t* pClassName = L"Hardware3D";
-	//Register Window Class
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	wc.hIconSm = nullptr;
-	RegisterClassEx(&wc);
-
-	//Create Window
-	HWND hWnd = CreateWindowEx(
-		0, pClassName,
-		L"Hardware3D",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200, 200, 640, 480,
-		nullptr, nullptr, hInstance, nullptr
-	);
-
-	//Show Window
-	ShowWindow(hWnd, SW_SHOW);
+	Window wnd(800, 600, L"Hardware 3D Engine");
 
 	//Message and loop to keep window open
 	MSG msg;
 	BOOL gResult;
-	while ( (gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	while ( (gResult = GetMessageW(&msg, nullptr, 0, 0)) > 0)
 	{
 		TranslateMessage(&msg);
 		DispatchMessageW(&msg);
